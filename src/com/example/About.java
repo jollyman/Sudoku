3cf367b7e2ef7604a3b0be9e99d2347a89ec3fbf
Andy <andresotta@gmail.com>
2011-12-27 13:18:24 +0200

    first commit

---
+++ src/com/example/About.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: a.sokolov
+ * Date: 12/27/11
+ * Time: 12:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class About {
+}
