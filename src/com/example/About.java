9538f3314174c3a2c593b0f112d7bf274c7ba140
Andy <andresotta@gmail.com>
2011-12-30 15:53:14 +0200

    dsfedf

--- src/com/example/About.java
+++ src/com/example/About.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by IntelliJ IDEA.
  * User: a.sokolov
@@ -7,5 +10,13 @@ package com.example;
  * Time: 12:54 PM
  * To change this template use File | Settings | File Templates.
  */
-public class About {
+public class About extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
 }
--- src/com/example/Sudoku.java
+++ src/com/example/Sudoku.java
@@ -1,9 +1,14 @@
 package com.example;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
-public class Sudoku extends Activity
+public class Sudoku extends Activity implements View.OnClickListener
 {
     /** Called when the activity is first created. */
     @Override
@@ -11,5 +16,41 @@ public class Sudoku extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        // Set up click listeners for all the buttons
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
+    }
+    // ...
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.about_button:
+                Intent i = new Intent(this, About.class);
+                startActivity(i);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                startActivity(new Intent(this, Prefs.class));
+                return true;
+        }
+        return false;
     }
 }
